238760db04cf431eab51859d16fb73dae0590f4d
pchmc72462 <pchmc72462@gmail.com>
2018-08-19 22:20:33 -0700

    Take out unnecessary things in if-else

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,9 @@ $(document).ready(function() {
    event.preventDefault();
    var varOne = ($("#input").val());
    $("#result").text(moduloCal());
-   
+
 // BACK END
 function moduloCal() {
-  var varOneString = varOne.toString();
   var modulo = varOne % 3;
   var one = varOne.match(/[1]/g);
   var zero = varOne.match(/[0]/g);
@@ -15,10 +14,10 @@ function moduloCal() {
   if (modulo === 0) {
     var modResult = varOne.replace(varOne, "Sorry Dave" );
     return modResult }
-  else if ((modulo !== 0) && (one > 0)) {
+  else if (one > 0) {
     var oneLook = varOne.replace(varOne, "boop" );
     return oneLook }
-  else if ((modulo !== 0) && (one = 0) && (zero > 0)) {
+  else if (zero > 0) {
     var zeroLook = varOne.replace(varOne, "beep" );
     return zeroLook }
   else {
