f3ad45db793c630cb5eaf46324ce074b5da6dfca
pchmc72462 <pchmc72462@gmail.com>
2018-08-19 22:04:07 -0700

    Redesign the scripts.js file. One bug with the zero variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,32 @@
-
-
-// BACK END
-var result = " "
- var underScores = function(string) {
-  var addTheUnders = 3 - string.length;
-  for (var i = 0; i<addTheUnders; i++) {
-    string= "_" + string;
-    console.log(string);
-  };
-
-var pos0 = string.charAt(0);
-var pos1 = string.charAt(1);
-var pos2 = string.charAt(2);
-console.log(pos0);
-console.log(pos1);
-console.log(pos2);
-
-var splits = input.split("");
-
-splits.forEach(function(split, index) {
-  if (split === "0") {
-    splits[index] = "beep"
-  } else {
-}
-
-var result = " "
- var underScores = function(string) {
-  var addTheUnders = 3 - string.length;
-  for (var i = 0; i<addTheUnders; i++) {
-    string= "_" + string;
-    console.log(string);
-  };
-};
-});
-};
-
 // FRONT END
 $(document).ready(function() {
  $("#form").submit(function(event) {
    event.preventDefault();
-   result = "";
-   var input = ($("#input").val());
-   underScores(input);
-   $("#result").text(splits.join(""));
+   var varOne = ($("#input").val());
+   $("#result").text(moduloCal());
+   
+// BACK END
+function moduloCal() {
+  var varOneString = varOne.toString();
+  var modulo = varOne % 3;
+  var one = varOne.match(/[1]/g);
+  var zero = varOne.match(/[0]/g);
+
+  if (modulo === 0) {
+    var modResult = varOne.replace(varOne, "Sorry Dave" );
+    return modResult }
+  else if ((modulo !== 0) && (one > 0)) {
+    var oneLook = varOne.replace(varOne, "boop" );
+    return oneLook }
+  else if ((modulo !== 0) && (one = 0) && (zero > 0)) {
+    var zeroLook = varOne.replace(varOne, "beep" );
+    return zeroLook }
+  else {
+    return varOne
+  }
+}
+
+
+
  });
 });
