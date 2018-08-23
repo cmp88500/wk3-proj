db5ff460ca74ceb5014cdf77b612197e6a701dcb
pchmc72462 <pchmc72462@gmail.com>
2018-08-22 18:51:27 -0700

    Comment out old code. Prepare to merge to maater.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,37 @@
-// FRONT END
-$(document).ready(function() {
- $("#form").submit(function(event) {
-   event.preventDefault();
-   var varOne = ($("#input").val());
-   $("#result").text(moduloCal());
-
-// BACK END
-function moduloCal() {
-  var modulo = varOne % 3;
-  var one = varOne.match(/[1]/g);
-  var zero = varOne.match(/[0]/g);
-
-  if (modulo === 0) {
-    var modResult = varOne.replace(varOne, "Sorry Dave" );
-    return modResult }
-  else if (one > 0) {
-    var oneLook = varOne.replace(varOne, "boop" );
-    return oneLook }
-  else if (zero > 0) {
-    var zeroLook = varOne.replace(varOne, "beep" );
-    return zeroLook }
-  else {
-    return varOne
-  }
-}
 
 
 
- });
-});
+
+// // BACK END
+// function moduloCal() {
+//   var modulo = varOne % 3;
+//   var one = varOne.match(/[1]/g);
+//   var zero = varOne.match(/[0]/g);
+//   console.log(one);
+//   if (modulo === 0) {
+//     var modResult = varOne.replace(varOne, "Sorry Dave" );
+//     return modResult }
+//   else if (one > 0) {
+//     var oneLook = varOne.replace(varOne, "boop" );
+//     return oneLook }
+//   else if (zero > 0) {
+//     var zeroLook = varOne.replace(varOne, "beep" );
+//     return zeroLook }
+//   else {
+//     return varOne
+//   }
+// }
+//
+//
+// // FRONT END
+// $(document).ready(function() {
+//  $("#form").submit(function(event) {
+//    event.preventDefault();
+//    var varOne = ($("#input").val());
+//    $("#result").text(moduloCal());
+//
+//
+//
+//
+//  });
+// });
