1a86c8fb962e5e9aef283833168380365bc5f391
pchmc72462 <pchmc72462@gmail.com>
2018-08-23 02:06:54 -0700

    Redo readme.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,33 @@
-  //BUSINESS LOGIC
-  var generate = function(num) {
-    var numbers = [];
+//BUSINESS LOGIC
+var generate = function(num) {
+  var numbers = [];
 
-    for (var i=0; i <= num; i++) {
-      if (i % 3 === 0) {
-        numbers.splice(i, 0, "I'm sorry, Dave. I'm afraid I can't do that.");
-      }
-      else if (i.toString().includes("1")) {
-        numbers.splice(i, 0, "Boop!");
-      }
-      else if (i.toString().includes("0")) {
-        numbers.splice(i, 0, "Beep!");
-      }
-      else {
-        numbers.push(i);
-      }
+  for (var i=0; i <= num; i++) {
+    if (i % 3 === 0) {
+      numbers.splice(i, 0, "I'm sorry, Dave. I'm afraid I can't do that.");
     }
-
-    return numbers;
+    else if (i.toString().includes("1")) {
+      numbers.splice(i, 0, "Boop!");
+    }
+    else if (i.toString().includes("0")) {
+      numbers.splice(i, 0, "Beep!");
+    }
+    else {
+      numbers.push(i);
+    }
   }
+  return numbers;
+}
 
-  //FRONT END LOGIC
-  $(document).ready(function() {
-    $("#form").submit(function(event) {
-      event.preventDefault();
-      $("#result").empty()
-      var input = parseInt($("#input").val());
-      var loops = generate(input);
-      loops.forEach(function(loop) {
-        $("#result").append("<li><span class='list'>" + loop + "</span></li>")
-      });
+//FRONT END LOGIC
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    $("#result").empty()
+    var input = parseInt($("#input").val());
+    var loops = generate(input);
+    loops.forEach(function(loop) {
+      $("#result").append("<li><span class='list'>" + loop + "</span></li>")
     });
-    });
+  });
+});
--- readme.md
+++ readme.md
@@ -4,10 +4,17 @@
 
 ## Description
 
-This is a page for running a beep boop loop with integers from 0 to 100.
-** Numbers with a 0 integer will change to the string "beep", unless...
-** Numbers with a 1 integer will change to the string "boop", unless...
-** Numbers divisible by 3 will give the string "I'm sorry, Dave. I'm afraid I can't do that."
+This is a page for running a beep boop loop.
+
+## Specifications
+### Input: "0"
+#### Output "Beep!"
+
+### Input: "1"
+#### Output: "Boop!"
+
+### Input: Any number divisible by 3
+#### Output: "I'm sorry, Dave. I'm afraid I can't do that."
 
 ## Languages Used
 ** HTML
@@ -17,8 +24,8 @@ This is a page for running a beep boop loop with integers from 0 to 100.
 
 ## Setup
 
-Open the GitHub and do it much better than I did.
+Open the GitHub and enter some numbers.
 
 ## Copyright
 
-MIT, but use at your own risk.
+MIT
