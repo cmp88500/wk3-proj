d29dac968319804b79d3f2c1c6cd99cd9dc3baf3
pchmc72462 <pchmc72462@gmail.com>
2018-08-22 19:21:21 -0700

    Develop function to find the number 0 in an integer converted to string.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,22 @@ function generate(num) {
   var numbers = [];
   for (var i = -1; i < num; i++) {
     numbers.push(i + 1);
-    console.log(numbers);
+    // console.log(numbers);
   }
     return numbers
   }
 generate(15);
 generate(5);
+
+function change(number) {
+  var numbers2 = [];
+  var string = number.toString();
+  var breaker = string.slice();
+  var zero = breaker.includes("0");
+  console.log(zero)
+}
+
+change(20);
+change(25);
+change(0);
+change(1);
