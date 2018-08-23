5f9d2d9921021bcf17af110fcbb515321da69168
pchmc72462 <pchmc72462@gmail.com>
2018-08-22 23:46:38 -0700

    Create if-else if statement for two more exceptions in loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,57 @@
 function generate(num) {
   var numbers = [];
-  for (var i = -1; i < num; i++) {
-    numbers.push(i + 1);
-    // console.log(numbers);
-  }
-    return numbers
-  }
-
-
-function change(number) {
-  var string = number.toString();
+  var string = num.toString();
   var zero = string.includes("0");
   var one = string.includes("1");
 
-  if (number % 3 === 0) {
-    var sorry = string.replace(number, "I'm sorry, Dave. I'm afraid I can't do that.");
-    console.log(sorry);
-    return sorry
+  for (var i=0; i <= num; i++) {
+    if (i % 3 === 0) {
+    numbers.splice(i, 0, "Sorry");
   }
-  else if (one === true) {
-    var boop = string.replace(number, "boop");
-    console.log(boop);
-    return boop
+    else if (i.toString().includes("1")) {
+    numbers.splice(i, 0, "Boop!");
   }
-  else if (zero === true) {
-    var beep = string.replace(number, "beep");
-    console.log(beep);
-    return beep
+    else if (i.toString().includes("0")) {
+    numbers.splice(i, 0, "Beep!");
   }
-  else {
-    console.log(number);
-    return number
-  }
-}
+    else {
+    numbers.push(i);
+    }
 
-change(10);
-change(20);
-change(32);
-change(21);
+    console.log(numbers);
+    }
+  }
+
+  generate(30);
+
+
+// function change(number) {
+  // var string = number.toString();
+  // var zero = string.includes("0");
+  // var one = string.includes("1");
+//
+//   if (number % 3 === 0) {
+//     var sorry = string.replace(number, "I'm sorry, Dave. I'm afraid I can't do that.");
+//     console.log(sorry);
+//     return sorry
+//   }
+//   else if (one === true) {
+//     var boop = string.replace(number, "boop");
+//     console.log(boop);
+//     return boop
+//   }
+//   else if (zero === true) {
+//     var beep = string.replace(number, "beep");
+//     console.log(beep);
+//     return beep
+//   }
+//   else {
+//     console.log(number);
+//     return number
+//   }
+// }
+//
+// change(10);
+// change(20);
+// change(32);
+// change(21);
