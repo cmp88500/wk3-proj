830034b10911798c614e9da5d23e910d9f6b75df
pchmc72462 <pchmc72462@gmail.com>
2018-08-23 01:08:41 -0700

    Add reset function. Delete unnecessary variables. Fixed indentions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,34 @@
-//BUSINESS LOGIC
-var generate = function(num) {
-  var numbers = [];
-  var string = num.toString();
-  var zero = string.includes("0");
-  var one = string.includes("1");
+  //BUSINESS LOGIC
+  var generate = function(num) {
+    var numbers = [];
 
-  for (var i=0; i <= num; i++) {
-    if (i % 3 === 0) {
-    numbers.splice(i, 0, "Sorry");
-  }
-    else if (i.toString().includes("1")) {
-    numbers.splice(i, 0, "Boop!");
-  }
-    else if (i.toString().includes("0")) {
-    numbers.splice(i, 0, "Beep!");
-  }
-    else {
-    numbers.push(i);
+    for (var i=0; i <= num; i++) {
+      if (i % 3 === 0) {
+        numbers.splice(i, 0, "Sorry");
+      }
+      else if (i.toString().includes("1")) {
+        numbers.splice(i, 0, "Boop!");
+      }
+      else if (i.toString().includes("0")) {
+        numbers.splice(i, 0, "Beep!");
+      }
+      else {
+        numbers.push(i);
+      }
     }
-    }
-    console.log(numbers);
+
     return numbers;
   }
 
-generate(48);
-
-//FRONT END LOGIC
-$(document).ready(function() {
-  $("#form").submit(function(event) {
-    event.preventDefault();
-    var input = parseInt($("#input").val());
-    var loops = generate(input);
-    loops.forEach(function(loop) {
-    $("#result").append("<li><span class='list'>" + loop + "</span></li>")
-  });
-});
-});
+  //FRONT END LOGIC
+  $(document).ready(function() {
+    $("#form").submit(function(event) {
+      event.preventDefault();
+      $("#result").empty()
+      var input = parseInt($("#input").val());
+      var loops = generate(input);
+      loops.forEach(function(loop) {
+        $("#result").append("<li><span class='list'>" + loop + "</span></li>")
+      });
+    });
+    });
