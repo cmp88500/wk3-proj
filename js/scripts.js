f31b8bd862cb453cd0c66320a0ecaf6b45b978b3
pchmc72462 <pchmc72462@gmail.com>
2018-08-22 21:08:05 -0700

    Create loop. However, modulo string in wrong places.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,49 @@
+// function change(number) {
+
+
+
 function generate(num) {
-  var numbers = [];
-  for (var i = -1; i < num; i++) {
-    numbers.push(i + 1);
-    // console.log(numbers);
-  }
-    return numbers
-  }
-
-
-function change(number) {
-  var string = number.toString();
+  var string = num.toString();
   var zero = string.includes("0");
   var one = string.includes("1");
-
-  if (number % 3 === 0) {
-    var sorry = string.replace(number, "I'm sorry, Dave. I'm afraid I can't do that.");
-    console.log(sorry);
-    return sorry
-  }
-  else if (one === true) {
-    var boop = string.replace(number, "boop");
-    console.log(boop);
-    return boop
-  }
-  else if (zero === true) {
-    var beep = string.replace(number, "beep");
-    console.log(beep);
-    return beep
-  }
-  else {
-    console.log(number);
-    return number
+  var numbers = [];
+  for (var i = -1; i <= num; i++) {
+    if (i % 3 === 0) {
+      numbers.splice(i + 1, 0, "Sorry"); }
+    else {
+      numbers.push(i + 1);
+    }
+    console.log(numbers);
   }
 }
 
-change(10);
-change(20);
-change(32);
-change(21);
+generate(15);
+
+
+
+
+//   if (number % 3 === 0) {
+//     var sorry = string.replace(number, "I'm sorry, Dave. I'm afraid I can't do that.");
+//     console.log(sorry);
+//     return sorry
+//   }
+//   else if (one === true) {
+//     var boop = string.replace(number, "boop");
+//     console.log(boop);
+//     return boop
+//   }
+//   else if (zero === true) {
+//     var beep = string.replace(number, "beep");
+//     console.log(beep);
+//     return beep
+//   }
+//   else {
+//     console.log(number);
+//     return number
+//   }
+// }
+//
+// change(10);
+// change(20);
+// change(32);
+// change(21);
